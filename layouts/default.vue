c705c9ac7b5028885988afb0f19434ca0541bf03
Ivan Hridniev <i.ivan.gridnev@gmail.com>
2024-07-15 23:02:17 +0800

    Hide alpha warning.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <img :src="GradientTop" alt="" class="select-none pointer-events-none absolute top-0 right-0" width="1113" height="1513" />
     <img :src="GradientBottom" alt="" class="select-none pointer-events-none absolute bottom-0 left-0" width="1400" height="1813" />
 
-    <div class="w-full absolute lg:top-9 top-0 z-10">
+    <div v-if="false" class="w-full absolute lg:top-9 top-0 z-10">
       <div class="lg:w-fit w-full mx-auto px-4 py-1 lg:py-2 bg-red-500 text-white text-xs text-center lg:rounded-full">
         We are not recommend to use Vueless UI in production, yet.
       </div>
